98aebb26e1d0fbaa0ded94efaa08bb52f4327d42
svt13 <svt13@ukr.net>
2023-07-12 19:42:08 +0300

    add bank account

---
+++ src/main/java/com/savita/machi_koro/models/game/BankAccount.java
@@ -0,0 +1,33 @@
+package com.savita.machi_koro.models.game;
+
+import com.savita.machi_koro.models.cards.Cards;
+
+public class BankAccount {
+    private int amount;
+    private int depositCount;
+    public void increase(int value) {
+        amount += value;
+    }
+
+    public void decrease(int value) {
+        amount = Math.max(amount - value, 0);
+    }
+    public int getAmount() {
+        return amount;
+    }
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+    public int getDepositCount() {
+        return depositCount;
+    }
+    public boolean deposit() {
+        if(amount > 0) {
+            depositCount++;
+            amount--;
+            return true;
+        }
+
+        return false;
+    }
+}
